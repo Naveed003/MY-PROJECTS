4b60fc1b39321bd7fc58bbbb9f1672cfdd505a95
naveed <imnaveed2003@gmail.com>
2020-05-07 13:47:20 +0400

    added

--- list-to-string-and-string-to-list.py
+++ list-to-string-and-string-to-list.py
@@ -1,26 +1,29 @@
-l = eval(input('enter the list(if empty input[])'))
-s = str(input("enter the string(if empty enter ' ')"))
-lenlist = len(l)
-lenstr = len(s)
+while True:
+    l = eval(input('enter the list(if empty input[])'))
+    s = str(input("enter the string(if empty enter ' ')"))
+    lenlist = len(l)
+    lenstr = len(s)
 
 
-def listtostring():
-    str1 = ''
-    for i in range(lenlist):
-        a = str(l[i])
-        str1 = str1 + str(l[i])
-    print(str1)
+    def listtostring():
+        str1 = ''
+        for i in range(lenlist):
+            a = str(l[i])
+            str1 = str1 + str(l[i])
+        print(str1)
 
 
-def stringtolist():
-    for i in range(lenstr):
-        a = s[i]
-        l.append(a)
-    print(l)
+    def stringtolist():
+        for i in range(lenstr):
+            a = s[i]
+            l.append(a)
+        print(l)
 
 
-if l == []:
-    stringtolist()
+    if l == []:
+        stringtolist()
 
-else:
-    listtostring()
+    else:
+        listtostring()
+    print('='*50)
+
